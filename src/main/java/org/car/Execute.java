9757387403734636b547040dff6cdda898cb5ab7
Nico1808 <niconiebisch@gmail.com>
2023-07-16 03:09:08 +0200

    aölsdjf

--- src/main/java/org/car/Execute.java
+++ src/main/java/org/car/Execute.java
@@ -37,13 +37,13 @@ public class Execute {
         RentalCar r = new RentalCar();
 
         while (true) {
-            try{
+            try {
 
                 //byte data of UDP message
                 byte[] messageData;
 
                 //byte UDP Message
-                byte [] parsedMessage;
+                byte[] parsedMessage;
                 byte[] buffer = new byte[65507];
                 //DatagramPacket for receiving data
                 DatagramPacket dgPacketIn = new DatagramPacket(buffer, buffer.length);
@@ -64,7 +64,7 @@ public class Execute {
                 //store originPort of the requesting travelBroker
                 int originPort = dataObject.getOriginPort();
 
-                switch (dataObject.getOperation()){
+                switch (dataObject.getOperation()) {
                     case PREPARE -> {
                         //log prepare received but not answered
                         transactionContext = new TransactionContext(States.PREPARE, originPort, false);
@@ -102,7 +102,7 @@ public class Execute {
                         LOGGER.log(Level.INFO, "2PC: Commit - " + transactionId);
 
                         //run actual commit and check if transaction was completed successfully if not, no answer will be sent
-                        if(r.commit(dataObject.getTransaktionNumber())) {
+                        if (r.commit(dataObject.getTransaktionNumber())) {
                             //create and parse response
                             responseMessage = new UDPMessage(dataObject.getTransaktionNumber(), SendingInformation.RENTALCAR, Operations.OK);
                             parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
@@ -125,45 +125,53 @@ public class Execute {
 
                         LOGGER.log(Level.INFO, "2PC: Abort - " + transactionId);
 
-                        //run actual abort and check if aborted successfully
-                        if(r.abort(dataObject.getTransaktionNumber())){
-                            //prepare answer
-                            responseMessage = new UDPMessage(dataObject.getTransaktionNumber(), SendingInformation.RENTALCAR, Operations.OK);
-                            parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
+                        transactionContext = transactionContextMap.get(transactionId);
 
-                            //send response to corresponding travelBroker instance
-                            DatagramPacket dgOutAbort = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, originPort);
-                            r.dgSocket.send(dgOutAbort);
+                        //check if there is an transaction Context because we already send a abort if received from one participant.
+                        //there could be a chance that this participant also returned abort to the prepare request which would lead to no entry for this transactionID
+                        //in our transaction ContextMap
+                        if (transactionContext != null) {
+                            //run actual abort and check if aborted successfully
+                            if (r.abort(dataObject.getTransaktionNumber())) {
+                                //prepare answer
+                                responseMessage = new UDPMessage(dataObject.getTransaktionNumber(), SendingInformation.RENTALCAR, Operations.OK);
+                                parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
 
-                            //log abort answered
-                            transactionContext = new TransactionContext(States.ABORT, originPort, true);
-                            logWriter.write(transactionId, transactionContext);
-                            transactionContextMap.put(transactionId, transactionContext);
+                                //send response to corresponding travelBroker instance
+                                DatagramPacket dgOutAbort = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, originPort);
+                                r.dgSocket.send(dgOutAbort);
+
+                                //log abort answered
+                                transactionContext = new TransactionContext(States.ABORT, originPort, true);
+                                logWriter.write(transactionId, transactionContext);
+                                transactionContextMap.put(transactionId, transactionContext);
+                            }
                         }
                     }
-                    case AVAILIBILITY -> {
-                        LOGGER.log(Level.INFO, "Availability: request from travelBroker");
 
-                        //parse availability data back to class
-                        messageData = dataObject.getData();
-                        AvailabilityData availabilityData = objectMapper.readValue(messageData, AvailabilityData.class);
+                case AVAILIBILITY -> {
+                    LOGGER.log(Level.INFO, "Availability: request from travelBroker");
 
-                        //run actual availability check with requested params and store available rooms
-                        ArrayList<Object> availableItems = r.getAvailableItems(availabilityData.getStartDate(), availabilityData.getEndDate());
+                    //parse availability data back to class
+                    messageData = dataObject.getData();
+                    AvailabilityData availabilityData = objectMapper.readValue(messageData, AvailabilityData.class);
 
-                        //if rooms not null prepare data and send answer
-                        if(!(availableItems == null)){
-                            byte[] parsedItems = objectMapper.writeValueAsBytes(availableItems);
-                            responseMessage = new UDPMessage(dataObject.getTransaktionNumber(), parsedItems, SendingInformation.RENTALCAR, Operations.AVAILIBILITY, Participant.rentalCarPort);
-                            parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
+                    //run actual availability check with requested params and store available rooms
+                    ArrayList<Object> availableItems = r.getAvailableItems(availabilityData.getStartDate(), availabilityData.getEndDate());
 
-                            //send response to corresponding travelBroker instance
-                            DatagramPacket dgOutAvailability= new DatagramPacket(parsedMessage, parsedMessage.length, r.localhost, originPort);
-                            r.dgSocket.send(dgOutAvailability);
+                    //if rooms not null prepare data and send answer
+                    if (!(availableItems == null)) {
+                        byte[] parsedItems = objectMapper.writeValueAsBytes(availableItems);
+                        responseMessage = new UDPMessage(dataObject.getTransaktionNumber(), parsedItems, SendingInformation.RENTALCAR, Operations.AVAILIBILITY, Participant.rentalCarPort);
+                        parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
 
-                        }
+                        //send response to corresponding travelBroker instance
+                        DatagramPacket dgOutAvailability = new DatagramPacket(parsedMessage, parsedMessage.length, r.localhost, originPort);
+                        r.dgSocket.send(dgOutAvailability);
+
                     }
                 }
+            }
 
             } catch (Exception e) {
                 LOGGER.log(Level.SEVERE, "The Socket or the objectMapper threw an error", e);
