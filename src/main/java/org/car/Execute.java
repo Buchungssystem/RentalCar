d562f2d91ddd682c1ecb7741c6318556fc1ab24c
Nico1808 <niconiebisch@gmail.com>
2023-07-16 22:15:15 +0200

    öalksdjf

--- src/main/java/org/car/Execute.java
+++ src/main/java/org/car/Execute.java
@@ -3,10 +3,14 @@ package org.car;
 import com.fasterxml.jackson.annotation.JsonIgnoreType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.mysql.cj.log.Log;
 import org.utils.*;
 
+import javax.xml.crypto.Data;
+import java.io.File;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -28,6 +32,8 @@ public class Execute {
 
     private static LogWriter<TransactionContext> logWriter = new LogWriter<>();
 
+    private static SharedRessourcesTimerThread sharedRessourcesTimerThread;
+
     public static void main(String[] args) {
 
         ObjectMapper objectMapper = new ObjectMapper();
@@ -35,6 +41,61 @@ public class Execute {
 
 
         RentalCar r = new RentalCar();
+
+        //recovery from crash
+
+        //check if logs are empty since we wouldn't need to recover if there are no logs
+        if(logWriter.isLogsNotEmpty()){
+            File dir = new File(logWriter.getDirectory());
+            File[] files = dir.listFiles();
+
+
+            //loop through all files of logs directory
+            for(File currentContextFile : files){
+
+                String name = currentContextFile.getName();
+                name = name.substring(0, name.length() - 4);
+
+                //write context back to contextMap
+                UUID transactionId = UUID.fromString(name);
+                transactionContext = logWriter.readLogFile(transactionId);
+                transactionContextMap.put(transactionId, transactionContext);
+
+                LOGGER.log(Level.INFO, "Transaction Context with the ID: " + transactionId + " was restored");
+            }
+
+            //loop through context map and react accordingly
+            for(Map.Entry<UUID, TransactionContext> entry : transactionContextMap.entrySet()){
+                //get transaction current context
+                TransactionContext currContext = entry.getValue();
+
+                UUID transactionId = entry.getKey();
+                switch(entry.getValue().getCurrentState()){
+                    case PREPARE -> {
+                        LOGGER.log(Level.INFO, "restore pepare was triggered");
+                        //the prepare was answered but the decision was never received
+
+                        //if we answered the prepare with abort there has nothing to be done. Since the coordinator decides abort based of our response.
+                        //we didn't set a booking since we answered with abort
+
+                        //if we answered commit we need to request what decission was made from the coordinator
+                        if(currContext.getDecission() == Operations.READY){
+                            UDPMessage message = new UDPMessage(transactionId, SendingInformation.RENTALCAR, Operations.REQUESTDECISION);
+
+                            try {
+                                byte[] parsedMessage = objectMapper.writeValueAsBytes(message);
+                                DatagramPacket dpRequestDecission = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, currContext.getCoordinatorPort());
+
+                                r.dgSocket.send(dpRequestDecission);
+                            }catch (Exception e){
+                                LOGGER.log(Level.SEVERE, "There was an error with sending the Request of the Decision - " + transactionId, e);
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
 
         while (true) {
             try {
@@ -66,11 +127,6 @@ public class Execute {
 
                 switch (dataObject.getOperation()) {
                     case PREPARE -> {
-                        //log prepare received but not answered
-                        transactionContext = new TransactionContext(States.PREPARE, originPort, false);
-                        logWriter.write(transactionId, transactionContext);
-                        transactionContextMap.put(transactionId, transactionContext);
-
                         LOGGER.log(Level.INFO, "2PC: Prepare - " + transactionId);
 
                         //get data of message
@@ -88,21 +144,52 @@ public class Execute {
 
                         r.dgSocket.send(dgOutPrepare);
 
+                        sharedRessourcesTimerThread = new SharedRessourcesTimerThread(false);
+
+                        //start timer thread
+                        //if there wasn't any Response received after 30 seconds the coordinator likely crashed, so we request if the other participant knows the response
+                        TimerThread timerThread = new TimerThread(transactionId, 30, Participant.rentalCarPort, false, sharedRessourcesTimerThread);
+                        timerThread.start();
+
                         //log prepare answered
-                        transactionContext = new TransactionContext(States.PREPARE, originPort, true);
+                        transactionContext = new TransactionContext(States.PREPARE, originPort, true, response, sharedRessourcesTimerThread);
                         logWriter.write(transactionId, transactionContext);
                         transactionContextMap.put(transactionId, transactionContext);
+
+
+                        //Fehlerfall 2
+                        //System.exit(0);
                     }
                     case COMMIT -> {
-                        //log commit received but not answered
-                        transactionContext = new TransactionContext(States.COMMIT, originPort, false);
-                        logWriter.write(transactionId, transactionContext);
-                        transactionContextMap.put(transactionId, transactionContext);
+                        //we received a response from coordinator, so we can cancel the timerThread
+                        sharedRessourcesTimerThread = transactionContextMap.get(transactionId).getSharedRessourcesTimerThread();
+
+                        sharedRessourcesTimerThread.setInterrupt(true);
 
                         LOGGER.log(Level.INFO, "2PC: Commit - " + transactionId);
+                        if(transactionContextMap.get(transactionId) != null){
+                            r.commit(dataObject.getTransaktionNumber());
+                            //create and parse response
+                            responseMessage = new UDPMessage(dataObject.getTransaktionNumber(), SendingInformation.RENTALCAR, Operations.OK);
+                            parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
 
-                        //run actual commit and check if transaction was completed successfully if not, no answer will be sent
-                        if (r.commit(dataObject.getTransaktionNumber())) {
+                            DatagramPacket dgOutCommit;
+
+                            //send response to corresponding travelBroker instance
+                            if(dataObject.isRecovery()){
+                                 dgOutCommit = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, transactionContextMap.get(transactionId).getCoordinatorPort());
+                            }else {
+                                 dgOutCommit = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, originPort);
+                            }
+
+                            r.dgSocket.send(dgOutCommit);
+
+                            //log delete logfile
+                            logWriter.delete(transactionId);
+                            transactionContextMap.remove(transactionId);
+                        }else {
+                            //recovery
+
                             //create and parse response
                             responseMessage = new UDPMessage(dataObject.getTransaktionNumber(), SendingInformation.RENTALCAR, Operations.OK);
                             parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
@@ -111,17 +198,16 @@ public class Execute {
                             DatagramPacket dgOutCommit = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, originPort);
                             r.dgSocket.send(dgOutCommit);
 
-                            //log commit answered
-                            transactionContext = new TransactionContext(States.COMMIT, originPort, true);
-                            logWriter.write(transactionId, transactionContext);
-                            transactionContextMap.put(transactionId, transactionContext);
+                            //log delete logfile
+                            logWriter.delete(transactionId);
+                            transactionContextMap.remove(transactionId);
                         }
                     }
                     case ABORT -> {
-                        //log abort but not answered
-                        transactionContext = new TransactionContext(States.ABORT, originPort, false);
-                        logWriter.write(transactionId, transactionContext);
-                        transactionContextMap.put(transactionId, transactionContext);
+                        //we received a response from coordinator, so we can cancel the timerThread
+                        sharedRessourcesTimerThread = transactionContextMap.get(transactionId).getSharedRessourcesTimerThread();
+
+                        sharedRessourcesTimerThread.setInterrupt(true);
 
                         LOGGER.log(Level.INFO, "2PC: Abort - " + transactionId);
 
@@ -131,46 +217,94 @@ public class Execute {
                         //there could be a chance that this participant also returned abort to the prepare request which would lead to no entry for this transactionID
                         //in our transaction ContextMap
                         if (transactionContext != null) {
-                            //run actual abort and check if aborted successfully
-                            if (r.abort(dataObject.getTransaktionNumber())) {
-                                //prepare answer
-                                responseMessage = new UDPMessage(dataObject.getTransaktionNumber(), SendingInformation.RENTALCAR, Operations.OK);
-                                parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
+                            r.abort(transactionId);
+                            //prepare answer
+                            responseMessage = new UDPMessage(transactionId, SendingInformation.RENTALCAR, Operations.OK);
+                            parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
 
-                                //send response to corresponding travelBroker instance
-                                DatagramPacket dgOutAbort = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, originPort);
-                                r.dgSocket.send(dgOutAbort);
+                            //send response to corresponding travelBroker instance
+                            DatagramPacket dgOutAbort = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, originPort);
+                            r.dgSocket.send(dgOutAbort);
 
-                                //log abort answered
-                                transactionContext = new TransactionContext(States.ABORT, originPort, true);
-                                logWriter.write(transactionId, transactionContext);
-                                transactionContextMap.put(transactionId, transactionContext);
-                            }
+                            //delete logfile
+                            logWriter.delete(transactionId);
+                            transactionContextMap.remove(transactionId);
+                        } else {
+                            //prepare answer
+                            responseMessage = new UDPMessage(transactionId, SendingInformation.RENTALCAR, Operations.OK);
+                            parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
+
+                            //send response to corresponding travelBroker instance
+                            DatagramPacket dgOutAbort = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, originPort);
+                            r.dgSocket.send(dgOutAbort);
+
+                            //delete logfile
+                            logWriter.delete(transactionId);
+                            transactionContextMap.remove(transactionId);
                         }
                     }
+                    case COORDINATORDOWN -> {
+                        LOGGER.log(Level.INFO, "Coordinator down");
+                        //Timer thread was finished and tells us to request the decision from other participant, since the coordinator likely crashed
 
-                case AVAILIBILITY -> {
-                    LOGGER.log(Level.INFO, "Availability: request from travelBroker");
-
-                    //parse availability data back to class
-                    messageData = dataObject.getData();
-                    AvailabilityData availabilityData = objectMapper.readValue(messageData, AvailabilityData.class);
-
-                    //run actual availability check with requested params and store available rooms
-                    ArrayList<Object> availableItems = r.getAvailableItems(availabilityData.getStartDate(), availabilityData.getEndDate());
-
-                    //if rooms not null prepare data and send answer
-                    if (!(availableItems == null)) {
-                        byte[] parsedItems = objectMapper.writeValueAsBytes(availableItems);
-                        responseMessage = new UDPMessage(dataObject.getTransaktionNumber(), parsedItems, SendingInformation.RENTALCAR, Operations.AVAILIBILITY, Participant.rentalCarPort);
+                        responseMessage = new UDPMessage(transactionId, SendingInformation.RENTALCAR, Operations.REQUESTDECISION);
                         parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
 
-                        //send response to corresponding travelBroker instance
-                        DatagramPacket dgOutAvailability = new DatagramPacket(parsedMessage, parsedMessage.length, r.localhost, originPort);
-                        r.dgSocket.send(dgOutAvailability);
+                        DatagramPacket dpRequestDecision = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, Participant.hotelPort);
+
+                        r.dgSocket.send(dpRequestDecision);
+                    }
+                    case REQUESTDECISION -> {
+                        LOGGER.log(Level.INFO, "Request decision was received");
+                        // the coordinator crashed and the other participant requests if this one already knows the decision
+                        Operations decision = r.requestDecision(transactionId);
+
+                        if(decision == null){
+                            //response was abort since there was no entry in the db
+                            responseMessage = new UDPMessage(transactionId, SendingInformation.RENTALCAR, Operations.ABORT, true);
+                            parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
+
+                            DatagramPacket dpDecisionAbort = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, Participant.hotelPort);
+
+                            r.dgSocket.send(dpDecisionAbort);
+                        }
+                        if(decision == Operations.COMMIT){
+                            //response was commit since there was a stable entry in the db
+                            responseMessage = new UDPMessage(transactionId, SendingInformation.RENTALCAR, decision, true);
+                            parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
+
+                            DatagramPacket dpDecisionAbort = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, Participant.hotelPort);
+
+                            r.dgSocket.send(dpDecisionAbort);
+                        }
+
+                        //otherwise we don't know the decission either and don't send an answer
+                        // the protocol is blocket till the coordinator is back life again
 
                     }
-                }
+                    case AVAILIBILITY -> {
+                        LOGGER.log(Level.INFO, "Availability: request from travelBroker");
+
+                        //parse availability data back to class
+                        messageData = dataObject.getData();
+                        AvailabilityData availabilityData = objectMapper.readValue(messageData, AvailabilityData.class);
+
+                        //run actual availability check with requested params and store available rooms
+                        ArrayList<Object> availableItems = r.getAvailableItems(availabilityData.getStartDate(), availabilityData.getEndDate());
+
+                        //if rooms not null prepare data and send answer
+                        if (!(availableItems == null)) {
+                            byte[] parsedItems = objectMapper.writeValueAsBytes(availableItems);
+                            responseMessage = new UDPMessage(dataObject.getTransaktionNumber(), parsedItems, SendingInformation.RENTALCAR, Operations.AVAILIBILITY, Participant.rentalCarPort);
+                            parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
+
+                            //send response to corresponding travelBroker instance
+                            DatagramPacket dgOutAvailability = new DatagramPacket(parsedMessage, parsedMessage.length, r.localhost, originPort);
+                            r.dgSocket.send(dgOutAvailability);
+                        }
+                        //erster fehlerfall testing
+                        //System.exit(0);
+                    }
             }
 
             } catch (Exception e) {
--- src/main/java/org/car/RentalCar.java
+++ src/main/java/org/car/RentalCar.java
@@ -60,7 +60,6 @@ public class RentalCar extends Participant{
             //Car is available
             stm = con.prepareStatement("INSERT INTO booking VALUES (\"" + transactionId + "\", \"" + startDate + "\", \"" + endDate + "\", 0, " + bookingData.getSelectedCar() + ")");
             stm.executeUpdate();
-            System.out.println("successfully booked");
 
             return Operations.READY;
         }catch (Exception e){
@@ -153,6 +152,31 @@ public class RentalCar extends Participant{
             return true;
         }
         return false;
+    }
+
+    public Operations requestDecision(UUID transactionId) {
+        DatabaseConnection dbConn = new DatabaseConnection();
+
+        try(Connection con = dbConn.getConn()){
+            PreparedStatement stm = con.prepareStatement("SELECT stable FROM booking WHERE bookingID = \"" + transactionId + "\"");
+            ResultSet rs = stm.executeQuery();
+            if(!rs.next()){
+                return null;
+            }
+
+            int stable = rs.getInt("stable");
+            if(stable == 1){
+                // return commit since transaction has been set to stable
+                return Operations.COMMIT;
+            }else{
+                // return prepare since we also only received prepare and got no response from it
+                return Operations.PREPARE;
+            }
+
+        }catch(Exception e){
+            LOGGER.log(Level.SEVERE, "Something went wrong with the database connection", e);
+            return Operations.PREPARE;
+        }
     }
 }
 
